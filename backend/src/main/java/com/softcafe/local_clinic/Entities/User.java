4930c68e0b6effbcbde227e57a98c1177cb74527
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-13 16:14:33 +0300

    feat: Added a class user that defines the base user for the system

---
+++ backend/src/main/java/com/softcafe/local_clinic/Entities/User.java
@@ -0,0 +1,56 @@
+package com.softcafe.local_clinic.Entities;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Inheritance(strategy = InheritanceType.JOINED)
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(name = "national_id", unique = true)
+    private String nationalId;
+
+    @Column(length = 100)
+    private String fullName;
+
+    @Column(name = "date_of_birth")
+    private LocalDate dateOfBirth;
+
+    @Column(name = "email_address", unique = true, length = 50)
+    private String emailAddress;
+
+    @Column(name = "phone_number", unique = true, nullable = false, length = 20)
+    private String phoneNumber;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updatedAt", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    public Gender gender;
+
+    @Column(name = "home_address")
+    private String address;
+}
