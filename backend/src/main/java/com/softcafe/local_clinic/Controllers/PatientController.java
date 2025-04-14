5cc34d7292e3c9afbfa530e185180770bba382cf
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-14 17:24:41 +0300

    feat (controller): Patient Controller
    Added a controller that processes requests/responses containing patient related operations. It currently exposes endpoints to add a patient and to search for a patient

---
+++ backend/src/main/java/com/softcafe/local_clinic/Controllers/PatientController.java
@@ -0,0 +1,101 @@
+package com.softcafe.local_clinic.Controllers;
+
+import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.softcafe.local_clinic.DTO.Patient.AddPatientDTO;
+import com.softcafe.local_clinic.DTO.Patient.SearchPatientDTO;
+import com.softcafe.local_clinic.Services.PatientService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/patient/")
+@RequiredArgsConstructor
+@Tag(name = "Patient Controller", description = "Handles the requests/responses that deal with the patient model")
+public class PatientController {
+
+    private final PatientService service;
+
+    @PostMapping
+    @Operation(
+            summary = "Adds a patient to the system",
+            description = "Receives the patient's data from the frontend and passes it to the service class to be added to the database"
+    )
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", description = "Patient successfully added",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))),
+
+        @ApiResponse(responseCode = "400", description = "Invalid input or missing fields",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))),
+
+        @ApiResponse(responseCode = "409", description = "Duplicate patient data",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))),
+
+        @ApiResponse(responseCode = "500", description = "Server error",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class)))
+    })
+    public ResponseEntity<Map<String, String>> addPatient(
+            @RequestBody(
+                    description = "The new patients data",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = AddPatientDTO.class))
+            )
+            @org.springframework.web.bind.annotation.RequestBody AddPatientDTO patientDTO
+    ) {
+        return service.add(patientDTO);
+    }
+
+    @Operation(
+            description = "Receives a patient's unique identifier to search for a patient in the system",
+            summary = "Searches for a patient"
+    )
+    @ApiResponses({
+        @ApiResponse(
+                responseCode = "200", description = "Patient found",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))
+        ),
+        @ApiResponse(
+                responseCode = "404", description = "Patient not found",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))
+        ),
+        @ApiResponse(
+                responseCode = "400", description = "Missing identifiers",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))
+        ),
+        @ApiResponse(
+                responseCode = "500", description = "Server error",
+                content = @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = Map.class))
+        )
+    })
+    @PostMapping("/search")
+    public ResponseEntity<Map<String, Object>> getPatient(
+            @RequestBody(
+                    description = "The patient's unique identifiers",
+                    required = true,
+                    content = @Content(schema = @Schema(implementation = SearchPatientDTO.class))
+            )
+            @org.springframework.web.bind.annotation.RequestBody SearchPatientDTO patientDTO
+    ) {
+        return service.find(patientDTO);
+    }
+}
