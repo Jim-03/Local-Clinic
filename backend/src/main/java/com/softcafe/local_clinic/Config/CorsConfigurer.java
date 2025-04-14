d3c536d7a6587a76601850e726e4ea2840c78f48
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-14 11:51:59 +0300

    feat (security): Added a configuration bean that allows the React frontend to perform CRUD operations on APIs

---
+++ backend/src/main/java/com/softcafe/local_clinic/Config/CorsConfigurer.java
@@ -0,0 +1,25 @@
+package com.softcafe.local_clinic.Config;
+
+
+import org.antlr.v4.runtime.misc.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfigurer {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/api/**")
+                        .allowedOrigins("http://localhost:5173")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
