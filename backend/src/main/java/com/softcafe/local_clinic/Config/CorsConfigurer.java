f826d7676f7eb15c966182aa70d305074f3b8dce
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-14 12:34:55 +0300

    refactor: Removed unused import

--- backend/src/main/java/com/softcafe/local_clinic/Config/CorsConfigurer.java
+++ backend/src/main/java/com/softcafe/local_clinic/Config/CorsConfigurer.java
@@ -1,7 +1,6 @@
 package com.softcafe.local_clinic.Config;
 
 
-import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
