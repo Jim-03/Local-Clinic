51a767603d6121d89bb7b19f16ec2da4e445a88d
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-25 18:26:40 +0300

    feat (View): Receptionist Home
    Added the home view for the receptionist's dashboard

---
+++ frontend/src/Views/Receptionist/Home/Home.jsx
@@ -0,0 +1,101 @@
+import styles from "./Home.module.css"
+import {useEffect, useState} from "react";
+import {FaHandshake, FaUser} from "react-icons/fa";
+import {RiNumbersFill} from "react-icons/ri";
+
+function Home({firstName}) {
+
+    useEffect(() => {
+        // Fetch the list of incomplete appointments
+        fetch("http://localhost:8080/api/appointments/incomplete").
+        then(response => response.json()).
+        then(data => {
+            setIncompleteAppointments(data.data)
+        })
+
+        // Fetch total patients
+        fetch("http://localhost:8080/api/patients/total").
+        then(response => response.json()).
+        then(data => {
+            setActivePatients(data.data)
+        })
+        // Fetch daily appointments
+        fetch("http://localhost:8080/api/appointment/recent/total").
+        then(response => response.json()).
+        then(data => {
+            setTotalAppointments(data.data)
+        })
+        // Fetch appointments served today
+        const now = new Date()
+        const start = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0);
+        const end = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 23, 59, 59, 99);
+        fetch("http://localhost:8080/api/appointments", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            credentials: "include",
+            body: JSON.stringify({start, end})
+        }).
+        then(response => response.json()).
+        then(data => {
+            setTotalPatients(data.data)
+        })
+    })
+
+
+    const [totalPatients, setTotalPatients] = useState(0)
+    const [totalAppointments, setTotalAppointments] = useState(0)
+    const [activePatients, setActivePatients] = useState(0)
+    const [recentAppointments, setIncompleteAppointments] = useState([])
+
+    return (
+        <div className={styles.background}>
+            <section className={styles.title}>
+                <h1>Welcome {firstName || "Receptionist"}</h1>
+                <h2>{new Date().toLocaleDateString()}</h2>
+                <h2>{new Date().toLocaleTimeString()}</h2>
+            </section>
+            <section className={styles.overview}>
+                <span className={styles.overviewData}>
+                    <FaUser/>
+                    {totalPatients} patients served today
+                </span>
+                <span className={styles.overviewData}>
+                    <FaHandshake/>
+                    {totalAppointments} appointments made today
+                </span>
+                <span className={styles.overviewData}>
+                    <RiNumbersFill/>
+                    {activePatients} active patients
+                </span>
+            </section>
+            <section className={styles.actions}>
+                <section className={styles.appointmentSection}>
+                    <h2>Recent incomplete appointments</h2>
+                    {recentAppointments.length == 0 ? <ul><li>All appointments are completed</li></ul> :
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Patient</th>
+                                <th>Doctor</th>
+                                <th>Time</th>
+                                <th>Room</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {recentAppointments.map(appointment => 
+                                <td>
+                                    <tr>{appointment.patient.name}</tr>
+                                    <tr>{appointment.doctor.name}</tr>
+                                    <tr>{appointment.time}</tr>
+                                    <tr>{appointment.roomNumber}</tr>
+                                </td>
+                            )}
+                        </tbody>
+                    </table>}
+                </section> 
+            </section>
+        </div>
+    );
+}
+
+export default Home
