327ecb9b021bf637c5a340bc94619cedd10c6a0f
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-26 17:14:40 +0300

    feat: Added a path to the receptionist dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Pages/Login/Login";
+import ReceptionistDashboard from "./Pages/Dashboard/Receptionist";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/receptionist" element={<ReceptionistDashboard/>} />
       </Routes>
     </BrowserRouter>
   );
