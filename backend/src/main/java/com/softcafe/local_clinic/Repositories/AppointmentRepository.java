556ca4eb6c172b6b0db32f26f11c274e05f86326
Jimmy Chemuku <jimmycn03@gmail.com>
2025-04-29 12:00:07 +0300

    feat (repository): Appointment repository
    Added a repository that directly transacts to the appointments table in the database

---
+++ backend/src/main/java/com/softcafe/local_clinic/Repositories/AppointmentRepository.java
@@ -0,0 +1,13 @@
+package com.softcafe.local_clinic.Repositories;
+
+import com.softcafe.local_clinic.Entities.Appointment;
+import com.softcafe.local_clinic.Entities.AppointmentStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    List<Appointment> findByStatus(AppointmentStatus appointmentStatus);
+}
