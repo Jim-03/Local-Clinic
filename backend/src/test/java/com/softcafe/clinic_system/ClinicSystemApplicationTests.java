aa145add218df13d84262209d03b9427d8da71e3
Jimmy Chemuku <jimmycn03@gmail.com>
2025-05-24 09:44:40 +0300

    chore: Initialized the spring boot backend

---
+++ backend/src/test/java/com/softcafe/clinic_system/ClinicSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softcafe.clinic_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
