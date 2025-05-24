bc3beb8a748fe4a7a46f3741b9a4f189e8469e1b
Jimmy Chemuku <jimmycn03@gmail.com>
2025-05-24 10:49:03 +0300

    feat: User model
    Added a base user class where staff and patients inherit from

---
+++ backend/src/main/java/com/softcafe/clinic_system/entities/Gender.java
@@ -0,0 +1,9 @@
+package com.softcafe.clinic_system.entities;
+
+import io.swagger.oas.annotations.media.Schema;
+
+@Schema(description = "An enumerated type for gender")
+public enum Gender {
+    MALE,
+    FEMALE
+}
---
+++ backend/src/main/java/com/softcafe/clinic_system/entities/User.java
@@ -0,0 +1,66 @@
+package com.softcafe.clinic_system.entities;
+
+import io.swagger.oas.annotations.media.Schema;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Inheritance(strategy = InheritanceType.TABLE_PER_CLASS)
+@Schema(name = "user", description = "The base class for all users of the system")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue
+    @Schema(description = "The database's primary key", example = "1")
+    private Long id;
+
+    @Column(name = "full_name", nullable = false)
+    @Schema(description = "The user's full legal name", example = "John Doe")
+    private String fullName;
+
+    @Column(name = "email_address", unique = true)
+    @Schema(description = "The user's email address", example = "johnDoe@example.com")
+    private String email;
+
+    @Column(name = "phone_number", unique = true, nullable = false)
+    @Schema(description = "The user's phone number", example = "0712345678")
+    private String phone;
+
+    @Column(name = "national_id_number", unique = true, nullable = false)
+    @Schema(description = "The user's national ID card number", example = "14156366")
+    private String nationalId;
+
+    @Column(name = "address", nullable = false)
+    @Schema(description = "The location the user resides", example = "Street X, Nakuru")
+    private String address;
+
+    @Column(name = "date_of_birth", nullable = false)
+    @Schema(description = "The date the user was born", example = "2000-11-20")
+    private LocalDate dateOfBirth;
+
+    @Enumerated(EnumType.STRING)
+    @Schema(description = "The user's gender", example = "MALE")
+    private Gender gender;
+
+    @Column(name = "profile_image")
+    @Schema(description = "The user's profile image", example = "profile.jpg")
+    private String image;
+
+    @Column(name = "created_at", nullable = false)
+    @Schema(description = "The date and time the record was created", example = "2025-05-24T10:41:56.976249081")
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @Schema(description = "The date and time the record was last updated", example = "2025-05-24T10:41:56.976249081")
+    private LocalDateTime updatedAt;
+}
