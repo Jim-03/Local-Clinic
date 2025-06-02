fb56f7c7fc7f95d65e8ab81519882a5b2b1c6b57
Jimmy Chemuku <jimmycn03@gmail.com>
2025-06-02 11:59:00 +0300

    feat: Appointment Util
    Helper class to reduce module size

---
+++ backend/src/main/java/com/softcafe/clinic_system/utils/AppointmentUtil.java
@@ -0,0 +1,24 @@
+package com.softcafe.clinic_system.utils;
+
+import com.softcafe.clinic_system.dto.appointment.AppointmentData;
+import com.softcafe.clinic_system.entities.Appointment;
+
+public class AppointmentUtil {
+
+    /**
+     * Converts an appointment data to its DTO
+     *
+     * @param appointment Appointment data
+     * @return Appointment DTO
+     */
+    public static AppointmentData toDto(Appointment appointment) {
+        return new AppointmentData(
+                appointment.getId(),
+                PatientUtil.toDto(appointment.getPatient()),
+                StaffUtil.toDto(appointment.getDoctor()),
+                appointment.getStatus(),
+                appointment.getCreatedAt(),
+                appointment.getUpdatedAt()
+        );
+    }
+}
