8077acb51b4527c1a494151c7a18bacb26527275
Jimmy Chemuku <jimmycn03@gmail.com>
2025-06-02 11:56:54 +0300

    feat: Appointment Repository
    Added a layer that interacts with the appointment's table in the database

---
+++ backend/src/main/java/com/softcafe/clinic_system/repositories/AppointmentRepository.java
@@ -0,0 +1,21 @@
+package com.softcafe.clinic_system.repositories;
+
+import com.softcafe.clinic_system.entities.Appointment;
+import com.softcafe.clinic_system.entities.Patient;
+import com.softcafe.clinic_system.entities.Staff;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface AppointmentRepository extends JpaRepository<Appointment, Long> {
+    Page<Appointment> findByDoctor(Pageable pageable, Staff staff);
+
+    Page<Appointment> findByCreatedAtBetween(LocalDateTime start, LocalDateTime end, Pageable of);
+
+    Page<Appointment> findByPatient(Patient patient, Pageable pageable);
+}
