1bffbc825f240d9753758cdd7f25b28295fcabac
Jimmy Chemuku <jimmycn03@gmail.com>
2025-06-14 11:28:54 +0300

    feat: Added cors configuration to allow the frontend to access the backend

--- backend/src/main/java/com/softcafe/clinic_system/config/WebSecConfigurer.java
+++ backend/src/main/java/com/softcafe/clinic_system/config/WebSecConfigurer.java
@@ -1,23 +1,49 @@
 package com.softcafe.clinic_system.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
 public class WebSecConfigurer {
 
+    @Value("${app.cors.allowed-origin}")
+    private String allowedOrigin;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(registry ->
                         registry.anyRequest().permitAll()
                 )
                 .build();
     }
 
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        List<String> allowedOrigins = List.of(allowedOrigin.split(","));
+        allowedOrigins.forEach(origin -> configuration.addAllowedOrigin(origin));
+
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedOrigin(allowedOrigin);
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
 }
