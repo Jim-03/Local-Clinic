be667b39251933f059914a319bf6af777adba104
Jimmy Chemuku <jimmycn03@gmail.com>
2025-06-18 15:43:51 +0300

    feat: Log entity
    Added an entity used to log activities in the database

---
+++ backend/src/main/java/com/softcafe/clinic_system/entities/Log.java
@@ -0,0 +1,27 @@
+package com.softcafe.clinic_system.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "logs")
+public class Log {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column
+    private String action;
+
+    @Column
+    private LocalDateTime time;
+}
