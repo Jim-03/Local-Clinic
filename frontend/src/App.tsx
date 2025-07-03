eab26f20cd259bc9ab3c811fe58bf6972da19310
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-07-03 13:22:00 +0300

    refactor: Added separate routes to serve the login and the protected  components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { Toaster } from "react-hot-toast"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import {ProtectedRoute} from "./components/ProtectedRoute.tsx";
+import Login from "./pages/Login/Login.tsx";
 
 function App() {
     return (<>
         <Toaster position={"top-center"} reverseOrder={true} />
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<ProtectedRoute />} />
+                <Route path="/" element={<Login />} />
+                <Route path="/dashboard" element={<ProtectedRoute/>}/>
             </Routes>
         </BrowserRouter>
     </>)
