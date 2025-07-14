bc7aee7ecebbdc4e338325cca6ccf0dd3bb3d42a
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-07-14 11:25:24 +0300

    feat: Added a method to fetch logs made by a staff member

--- backend/src/main/java/com/softcafe/clinic_system/repositories/LogRepository.java
+++ backend/src/main/java/com/softcafe/clinic_system/repositories/LogRepository.java
@@ -1,9 +1,12 @@
 package com.softcafe.clinic_system.repositories;
 
 import com.softcafe.clinic_system.entities.Log;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LogRepository extends JpaRepository<Log, Long> {
+    Page<Log> findByStaff_Id(long id, Pageable pageable);
 }
