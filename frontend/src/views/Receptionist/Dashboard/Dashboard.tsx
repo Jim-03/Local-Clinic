3f071f52d6e750169beb6da0d737a83b23e72204
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-08-18 18:47:11 +0300

    feat(view): Added the receptionist's dashboard

---
+++ frontend/src/views/Receptionist/Dashboard/Dashboard.tsx
@@ -0,0 +1,100 @@
+import styles from "./Dashboard.module.css"
+import {type JSX, useEffect, useState} from "react";
+import {toast} from "react-hot-toast";
+import {TbLoaderQuarter} from "react-icons/tb";
+import {FaCalendarAlt, FaCalendarCheck, FaCalendarPlus, FaCalendarTimes, FaUserPlus} from "react-icons/fa";
+
+interface Log {
+    id: number;
+    action: string;
+    time: string;
+}
+
+/**
+ * A view component that renders the Receptionist's dashboard
+ * It displays statistics such as appointments(total, complete, incomplete) and recent logs from the user
+ * It provides quick actions to add new patients, book appointments and view previously booked appointments
+ */
+function Dashboard(): JSX.Element {
+    const [isLoading, setIsLoading] = useState(false)
+    const [appointmentsToday, setAppointmentsToday] = useState<number>(0);
+    const [completed, setCompleted] = useState<number>(0);
+    const [incomplete, setIncomplete] = useState<number>(0);
+    const [logs, setLogs] = useState<Log[]>([])
+    const api = import.meta.env.VITE_API_URL + "/api/statistics/receptionist";
+
+    function fetchStatistics(id: number): void {
+        setIsLoading(true)
+        fetch(`${api}/${id}`)
+            .then(async response => {
+                const data = await response.json()
+
+                if (!response.ok && response.status === 404) {
+                    toast.error("An error has occurred when fetching the statistics!")
+                    console.warn(data)
+                    return
+                }
+                return data
+            })
+            .then(data => {
+                setAppointmentsToday(data.total);
+                setCompleted(data.complete);
+                setIncomplete(data.incomplete);
+                setLogs(data.logData)
+            })
+            .catch(console.warn)
+            .finally(() => setIsLoading(false))
+    }
+
+    // Hook to fetch the receptionist's statistics
+    useEffect(() => {
+        const userData = JSON.parse(String(localStorage.getItem("userData")))
+        fetchStatistics(userData.id);
+    }, []);
+
+    return <div className={styles.background}>
+        {isLoading ?
+            <div className={styles.loadingDiv}><TbLoaderQuarter className={styles.loading}/><p>Loading ...</p></div> :
+            <>
+                <div className={styles.statistics}>
+                    <h3>Statistics</h3>
+                    <div>
+                        <section className={styles.stats}>
+                            <FaCalendarPlus/>
+                            <p>{appointmentsToday} appointments made today</p>
+                        </section>
+                        <section className={styles.stats}>
+                            <FaCalendarCheck/>
+                            <p>{completed} completed appointments</p>
+                        </section>
+                        <section className={styles.stats}>
+                            <FaCalendarTimes/>
+                            <p>{incomplete} incomplete appointments</p>
+                        </section>
+                    </div>
+                </div>
+                <div className={styles.recentActions}>
+                    <h3>Recent actions</h3>
+                    <ul className={styles.actionList}>
+                        {logs.length !== 0 ?
+                            logs.map(log => <li key={log.id}>
+                                {log.action}
+                            </li>)
+                            :
+                            <li> There are no recent actions</li>
+                        }
+                    </ul>
+                </div>
+                <div className={styles.quickActions}>
+                    <h3>Quick actions</h3>
+                    <div>
+                        <button className={styles.quickActionButton } onClick={() => alert("Feature to be implemented soon!")}><FaUserPlus/> Add new Patient</button>
+                        <button className={styles.quickActionButton } onClick={() => alert("Feature to be implemented soon!")}><FaCalendarPlus/> Create an appointment</button>
+                        <button className={styles.quickActionButton } onClick={() => alert("Feature to be implemented soon!")}><FaCalendarAlt/> View appointments</button>
+                    </div>
+                </div>
+            </>}
+    </div>
+}
+
+export default Dashboard
