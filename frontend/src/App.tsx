52c7ddf863588f20a485ede9d83b21d572b39479
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-08-23 17:24:42 +0300

    feat: Added a toaster notification

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router';
 import { type JSX } from 'react';
 import Login from './pages/login/Login.tsx';
 import ProtectedRoute from './ProtectedRoute.tsx';
+import { Toaster } from "react-hot-toast";
 
 function App(): JSX.Element {
   return <BrowserRouter>
@@ -10,6 +11,15 @@ function App(): JSX.Element {
       <Route path={"/login"} element={<Login/>}/>
     </Routes>
   </BrowserRouter>;
+    return <>
+        <Toaster position={"top-center"} reverseOrder={true}/>
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={<ProtectedRoute/>}/>
+                <Route path={"/login"} element={<Login/>}/>
+            </Routes>
+        </BrowserRouter>
+    </>;
 
 }
 
