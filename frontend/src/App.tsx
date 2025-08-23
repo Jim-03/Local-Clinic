e572daa30df465c74305c42830051f07c18e5efa
Jimmy Chemuku Nyongesa <jimmycn03@gmail.com>
2025-08-23 16:50:49 +0300

    refactor: Added the ProtectedRoute component to be served when visiting the default route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { type JSX } from 'react';
 import Login from './pages/login/Login.tsx';
+import ProtectedRoute from './ProtectedRoute.tsx';
 
 function App(): JSX.Element {
   return <BrowserRouter>
     <Routes>
+      <Route path={"/"} element={<ProtectedRoute/>}/>
       <Route path={"/login"} element={<Login/>}/>
     </Routes>
   </BrowserRouter>;
